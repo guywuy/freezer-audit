cfa4d64c9353214dd76060b0b6764cb93644d5dc
guy <gcschino@hotmail.com>
2025-01-12 21:43:26 +0000

    Fixes notes in export

--- app/routes/items.tsx
+++ app/routes/items.tsx
@@ -94,7 +94,7 @@ export default function ItemsPage() {
             title.replace(/,/g, ""),
             amount.replace(/,/g, ""),
             location.replace(/,/g, ""),
-            notes.replace(/,/g, ""),
+            notes ? notes.replace(/,/g, "") : "",
             category,
             needsMore ? "Yes" : "No",
           ].join(","),
@@ -115,7 +115,10 @@ export default function ItemsPage() {
     <div className="flex h-full min-h-screen flex-col">
       <header className="flex items-center justify-between p-4 pl-2">
         <details>
-          <summary className="text-black p-2 font-bold relative list-none [&::-webkit-details-marker]:hidden flex items-center gap-1" data-menu>
+          <summary
+            className="text-black p-2 font-bold relative list-none [&::-webkit-details-marker]:hidden flex items-center gap-1"
+            data-menu
+          >
             <svg height="32px" viewBox="0 0 32 32" width="32px">
               <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" />
             </svg>

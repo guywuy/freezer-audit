05fa77ab0c154b78821227f4ab5ecbf0800477a5
Guy <gcschino@hotmail.com>
2026-03-16 15:53:05 +0000

    Add preventScrollReset to NavLink in ChildLayout

--- app/components/ChildLayout.tsx
+++ app/components/ChildLayout.tsx
@@ -10,7 +10,7 @@ export default function ChildLayout({ children }: Props) {
     <div
       className={`z-10 h-screen fixed bottom-0 left-0 w-full  flex flex-col`}
     >
-      <NavLink to="/items" className="flex-1" title="Close" />
+      <NavLink to="/items" className="flex-1" title="Close" preventScrollReset />
       <div className="min-h-[60vh] h-auto border-t-4 border-l-4 p-6 rounded-tl-xl overflow-auto shadow-top bg-fuchsia-50 border-fuchsia-600 bg-noise ">
         {children}
       </div>

46b5fa6a8859dd9e4297d6448ad40dfee0b8c011
guy <gcschino@hotmail.com>
2026-03-21 06:43:26 +0000

    Move prisma to deps for CI

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
 #!/bin/sh -ex
 
-# This file is how Fly starts the server (configured in fly.toml). Before starting
-# the server though, we need to run any prisma migrations that haven't yet been
-# run, which is why this file exists in the first place.
-# Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
-
-# Run migrations and generate the client
+# Run migrations against the mounted SQLite volume
 npx prisma migrate deploy
-npx prisma generate
 
 # Start the application
 npm run start

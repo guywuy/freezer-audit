77856cfbcd2adbcc7988ce9775fe4ee8e2fa5b2e
guy <gcschino@hotmail.com>
2026-03-21 06:27:07 +0000

    Update start.sh to remove swap config and fix line endings

--- start.sh
+++ start.sh
@@ -5,14 +5,9 @@
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
-# allocate swap space
-fallocate -l 512M /swapfile
-chmod 0600 /swapfile
-mkswap /swapfile
-echo 10 > /proc/sys/vm/swappiness
-swapon /swapfile
-echo 1 > /proc/sys/vm/overcommit_memory
-
+# Run migrations and generate the client
 npx prisma migrate deploy
 npx prisma generate
+
+# Start the application
 npm run start

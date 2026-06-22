8fe67f0cb06c276ec3152f446a5c2599e3fb0df4
Guywuy <gcschino@hotmail.com>
2026-06-22 10:53:30 +0100

    Minor updates

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -79,14 +79,12 @@ function deleteUserByUsername(username: string) {
   cy.clearCookie("__session");
 }
 
-// We're waiting a second because of this issue happen randomly
-// https://github.com/cypress-io/cypress/issues/7306
 // Also added custom types to avoid getting detached
 // https://github.com/cypress-io/cypress/issues/7306#issuecomment-1152752612
 // ===========================================================
-function visitAndCheck(url: string, waitTime = 1000) {
+function visitAndCheck(url: string) {
   cy.visit(url);
-  cy.location("pathname").should("contain", url).wait(waitTime);
+  cy.location("pathname").should("contain", url);
 }
 
 export const registerCommands = () => {
